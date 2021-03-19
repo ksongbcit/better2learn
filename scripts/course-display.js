579370d2d7b5db032d8683223b442d64388c30b6
Avneet Sandhu <aksx97@gmail.com>
2021-03-19 19:04:24 -0400

    implemented onclick functions for courses in gradebook

---
+++ scripts/course-display.js
@@ -0,0 +1,70 @@
+//changing display when course is clicked
+function comp1100() {
+    $("#comp1100").css("display", "block");
+    $("#comp1113").css("display", "none");
+    $("#comm1116").css("display", "none");
+    $("#comp1510").css("display", "none");
+    $("#comp1537").css("display", "none");
+    $("#comp1712").css("display", "none");
+    $("#comp1800").css("display", "none");
+  }
+
+  function comp1113() {
+    $("#comp1100").css("display", "none");
+    $("#comp1113").css("display", "block");
+    $("#comm1116").css("display", "none");
+    $("#comp1510").css("display", "none");
+    $("#comp1537").css("display", "none");
+    $("#comp1712").css("display", "none");
+    $("#comp1800").css("display", "none");
+  }
+
+  function comm1116() {
+    $("#comp1100").css("display", "none");
+    $("#comp1113").css("display", "none");
+    $("#comm1116").css("display", "block");
+    $("#comp1510").css("display", "none");
+    $("#comp1537").css("display", "none");
+    $("#comp1712").css("display", "none");
+    $("#comp1800").css("display", "none");
+  }
+
+  function comp1510() {
+    $("#comp1100").css("display", "none");
+    $("#comp1113").css("display", "none");
+    $("#comm1116").css("display", "none");
+    $("#comp1510").css("display", "block");
+    $("#comp1537").css("display", "none");
+    $("#comp1712").css("display", "none");
+    $("#comp1800").css("display", "none");
+  }
+
+  function comp1537() {
+    $("#comp1100").css("display", "none");
+    $("#comp1113").css("display", "none");
+    $("#comm1116").css("display", "none");
+    $("#comp1510").css("display", "none");
+    $("#comp1537").css("display", "block");
+    $("#comp1712").css("display", "none");
+    $("#comp1800").css("display", "none");
+  }
+
+  function comp1712() {
+    $("#comp1100").css("display", "none");
+    $("#comp1113").css("display", "none");
+    $("#comm1116").css("display", "none");
+    $("#comp1510").css("display", "none");
+    $("#comp1537").css("display", "none");
+    $("#comp1712").css("display", "block");
+    $("#comp1800").css("display", "none");
+  }
+
+  function comp1800() {
+    $("#comp1100").css("display", "none");
+    $("#comp1113").css("display", "none");
+    $("#comm1116").css("display", "none");
+    $("#comp1510").css("display", "none");
+    $("#comp1537").css("display", "none");
+    $("#comp1712").css("display", "none");
+    $("#comp1800").css("display", "block");
+  }
