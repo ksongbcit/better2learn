100462c1ef907ff3bc8943d162a26490d2ffcb8e
Kyung Min Song <ksong34@my.bcit.ca>
2021-03-25 12:27:45 -0700

    added additional tabs for tasktracker.html started js file for adding items to the list

---
+++ scripts/tasktracker.js
@@ -0,0 +1,27 @@
+$(
+  const itemlist = document.getElementById("itemlist");
+  const button = document.getElementById("saveitem");
+
+  var itemname = document.getElementById("newitem").value;
+  var itemstart = document.getElementById("newitemstart").value;
+  var itemdue = document.getElementById("newitemdue").value;
+
+  function(e)
+
+
+
+  button.addEventListener("click", e);
+
+
+
+
+
+  console.log(itemname);
+  itemlist.appendChild(newitem);
+
+
+
+
+);
+
+
