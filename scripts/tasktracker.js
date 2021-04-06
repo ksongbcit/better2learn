aeed2b52c3c9ffa620e6dcb2a0541dfccc484ee0
Kyung Min Song <kyungminsong@gmail.com>
2021-04-06 00:14:40 -0700

    added logout function to task tracker, added functions to add and retrieve todo list complete and incomplete from database, moved showname function to main.js, added auth declaration to firebase-api.js

--- scripts/tasktracker.js
+++ scripts/tasktracker.js
@@ -1,46 +1,118 @@
-const itemlist = document.querySelector("#itemlist");
-const submit = document.querySelector("#saveitem");
+const save = document.getElementById('saveitem');
+const reset = document.getElementById('reset');
+const logout = document.getElementById('logout');
+const todotab = document.getElementById('nav-todo-tab');
+const donetab = document.getElementById('nav-done-tab');
+const statstab = document.getElementById('nav-stats-tab');
 
-var newname = document.querySelector("#newitem");
-var newstartdate = document.querySelector("#newitemstartdate");
-var newduedate = document.querySelector("#newitemduedate");
-var newstarttime = document.querySelector("#newitemstarttime");
-var newduetime = document.querySelector("#newitemduetime");
+function getInputsAndAdd() {
+  save.addEventListener('click', (e) => {
 
-submit.addEventListener("click", addNewItem);
+    e.preventDefault();
 
-var user = firebase.auth().currentUser;
+    firebase.auth().onAuthStateChanged(function (user) {
+      //get the input values
+      var title = document.getElementById('newitem').value;
+      var startdate = document.getElementById('newitemstartdate').value;
+      var starttime = document.getElementById('newitemstarttime').value;
+      var duedate = document.getElementById('newitemduedate').value;
+      var duetime = document.getElementById('newitemduetime').value;
 
-function addNewItem(e) {
-  e.preventDefault();
+      console.log(title);
+      console.log(startdate);
+      console.log(starttime);
 
-  let itemContainer = document.createElement('div');
-  let itemDetail = document.createElement('p');
-  let itemStart = document.createElement('p');
-  let itemDue = document.createElement('p');
-
-  itemDetail.innerText = "Task : " + newname.value;
-  itemStart.innerText = "Start on : " + newstartdate.value + "  At : " + newstarttime.value;
-  itemDue.innerText = "Finish on : " + newduedate.value + "  By : " + newduetime.value;
-
-  // if (newname.value != "" && newstartdate.value != "" && newstarttime.value != "" &&
-  //   newduedate.value != "" && newduetime.value != "") {
-    itemlist.appendChild(itemContainer);
-    itemContainer.appendChild(itemDetail);
-    itemContainer.appendChild(itemStart);
-    itemContainer.appendChild(itemDue);
-  // } else {
-  //   alert("Please fill in the blanks");
-  // }
-
-  var todolist = db.collection("todolist").doc(user.uid);
-
-  todoList.set({
-    Details: itemDetail.innerText,
-    Start: itemStart.innerText,
-    Finish: itemDue.innerText
-  }, {
-    merge: true
+      //save to database
+      db.collection('todolist').doc(user.uid).collection('items').add({
+        'title': title,
+        'startdate': startdate,
+        'starttime': starttime,
+        'duedate': duedate,
+        'duetime': duetime,
+        'complete': false
+      })
+      showTodoList('complete');
+    });
   });
+};
+getInputsAndAdd();
 
-}
+//show todo list
+function showTodoList(status) {
+  firebase.auth().onAuthStateChanged(function (user) {
+    db.collection('todolist')
+      .doc(user.uid)
+      .collection('items')
+      .where(status, '==', false)
+      .get()
+      .then(function (snap) {
+        snap.forEach(function (doc) {
+          let title = doc.data().title;
+          let startdate = doc.data().startdate;
+          let starttime = doc.data().starttime;
+          let duedate = doc.data().duedate;
+          let duetime = doc.data().duetime;
+
+          console.log(title);
+          console.log(startdate);
+          console.log(starttime);
+
+          $('#itemlist').append('<h4>' + title + '</h4>');
+          $('#itemlist').append('<h5>' + startdate + ' at ' + starttime + '</h5>');
+          $('#itemlist').append('<h5>' + duedate + ' at ' + duetime + '</h5>');
+
+        });
+      });
+  });
+};
+showTodoList('complete');
+
+
+
+// //show done list
+// function showDoneList(status) {
+//   firebase.auth().onAuthStateChanged(function (user) {
+//     db.collection('todolist')
+//       .doc(user.uid)
+//       .collection('items')
+//       .where(status, '==', true)
+//       .get()
+//       .then(function (snap) {
+//         snap.forEach(function (doc) {
+//           let title = doc.data().title;
+//           let startdate = doc.data().startdate;
+//           let starttime = doc.data().starttime;
+//           let duedate = doc.data().duedate;
+//           let duetime = doc.data().duetime;
+
+//           console.log(title);
+//           console.log(startdate);
+//           console.log(starttime);
+
+//           let item = document.createElement('div');
+//           item.appendChild('<h4>' + title + '</h4>');
+//           item.appendChild('<h5>' + startdate + " at " + starttime + '</h5>');
+//           item.appendChild('<h5>' + duedate + " at " + duetime + '</h5>');
+
+//           $('#itemlist').append(item);
+
+//         });
+//       });
+//   });
+// };
+// showDoneList('complete');
+
+
+function quit() {
+  logout.addEventListener('click', () => {
+
+    console.log("logging out user");
+    firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      window.location.href = "index.html";
+    }).catch((error) => {
+      // An error happened.
+    });
+  });
+};
+quit();
